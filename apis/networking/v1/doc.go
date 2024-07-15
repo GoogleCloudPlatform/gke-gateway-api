d588c93a2cd0682c2488f520bee1bb5100546376
Brian Kennedy <briankennedy@google.com>
2024-07-15 15:38:19 -0700

    Add register codegen.

--- apis/networking/v1/doc.go
+++ apis/networking/v1/doc.go
@@ -14,21 +14,11 @@
 *     limitations under the License.
 */
 
-// Package v1 contains API Schema definitions for the networking.gke.io ServicePolicy v1 API group.
+// Package v1 contains policy types for the GKE implementation of the Gateway
+// API.
+//
 // +kubebuilder:object:generate=true
 // +groupName=networking.gke.io
 package v1
 
-import (
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
-)
 
-var (
-	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "networking.gke.io", Version: "v1"}
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
-	// AddToScheme adds the types in this group-version to the given scheme.
-	AddToScheme = SchemeBuilder.AddToScheme
-)
